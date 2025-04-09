54cf7da43f5bb39c2eca65bfbab48ecac17e75ad
ArtemKonnov <odhinn3@gmail.com>
2025-04-09 22:51:11 +0500

    update

--- depthsum.py
+++ depthsum.py
@@ -1,7 +1,6 @@
 import arcpy
 import os
 
-# Настройки окружения
 arcpy.env.workspace = r"D:\Temp"
 arcpy.env.overwriteOutput = True
 
@@ -10,6 +9,7 @@ def depthsum(layer, distance, depth, field, output):
     try:
         with arcpy.da.UpdateCursor(layer, ["SHAPE@LENGTH", field]) as cursor:
             sum_depth = 0
+            sum_profiles = 0
             output_file = output + ".txt"
 
             mode = "w+" if os.path.isfile(output_file) else "w"
@@ -23,15 +23,14 @@ def depthsum(layer, distance, depth, field, output):
 
                     row[1] = (round(row[0] / distance) + 1) * depth
                     file.write(f"{round(row[0])} {row[1]}\n")
-                    arcpy.AddMessage(f"New row added: {round(row[0])} {row[1]}")
 
                     sum_depth += row[1]
-                    arcpy.AddMessage(f"Sum so far: {sum_depth}")
+                    sum_profiles += 1
 
                     cursor.updateRow(row)
                     arcpy.AddMessage("Row updated")
 
-                file.write(f"Total depth: {sum_depth}")
+                file.write(f"Total depth: {sum_depth} \n" + f"Number of profiles: {sum_profiles}")
                 arcpy.AddMessage(f"Total depth: {sum_depth}")
 
     except Exception as e:
@@ -40,7 +39,7 @@ def depthsum(layer, distance, depth, field, output):
 
 def main():
     try:
-        # Получение параметров
+
         l = arcpy.GetParameterAsText(0)
         d = int(arcpy.GetParameter(1))
         a = int(arcpy.GetParameter(2))
@@ -49,22 +48,19 @@ def main():
 
         arcpy.AddMessage("Parameters received successfully")
 
-        # Проверка на тип слоя
+
         desc = arcpy.Describe(l)
         if desc.shapeType != "Polyline":
             arcpy.AddError("Input feature class must be a Polyline")
             raise ValueError("Invalid geometry type")
 
-        # Проверка наличия поля
+
         fields = arcpy.ListFields(l)
         field_exists = any(field.name == f for field in fields)
-        arcpy.AddMessage("Field exists: " + str(field_exists))
 
         if not field_exists:
             arcpy.AddField_management(l, f, "LONG")
-            arcpy.AddMessage("Field created")
 
-        # Основной расчет
         depthsum(l, d, a, f, o)
 
     except Exception as e:
